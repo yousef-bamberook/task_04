f1cb952d5e0990da6448f067af6dccf2e0cc176a
yousef_bamberook <abamby2007@gmail.com>
2019-07-23 17:17:23 +0300

    create and open admin site

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    opening_time = models.TimeField(auto_now=False, auto_now_add=False)
+    closing_time = models.TimeField(auto_now=False, auto_now_add=False)
+
+
+def __str__(self):
+    return self.name
